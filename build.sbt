8e1f8ccd4857361f5ef3c22704d9aa6d25468c94
Hailin Cai <hailin.cai.neulion@gmail.com>
2021-11-30 19:18:40 -0500

    init version

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "advanced_scala.git"
+
+version := "0.1"
+
+scalaVersion := "3.0.2"
---
+++ src/main/scala/playground/Playground.scala
@@ -0,0 +1,7 @@
+package playground
+
+object Playground{
+  def main(arg: Array[String]): Unit = {
+    println("I'm so ready to become a rockstart.")
+  }
+}
